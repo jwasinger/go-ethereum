3c2b3012f49c26b49ee588394ad91a656669c159
Jared Wasinger <j-wasinger@hotmail.com>
2021-03-09 02:58:51 +0000

    modify test case to use bn128 ec group order

--- core/vm/arith384/arith384_test.go
+++ core/vm/arith384/arith384_test.go
@@ -53,13 +53,13 @@ func TestMulModMont(t *testing.T) {
 	var x, y, r_squared, expected, mod Element
 	var modinv uint64
 
-	// modulus = BN128 curve order, r = 1<<256 % modulus, r_squared = r**2 % modulus
-	mod = *ElementFromString("21888242871839275222246405745257275088696311157297823662689037894645226208583")
-	r_squared = *ElementFromString("3096616502983703923843567936837374451735540968419076528771170197431451843209")
-	modinv = 9786893198990664585
+	// modulus = order of group on BN128 curve , r = 1<<256 % modulus, r_squared = r**2 % modulus
+	mod = *ElementFromString("21888242871839275222246405745257275088548364400416034343698204186575808495617")
+	r_squared = *ElementFromString("944936681149208446651664254269745548490766851729442924617792859073125903783")
+	modinv = 14042775128853446655 // pow(-mod, -1, 1<<64)
 
-	x = *ElementFromString("3")
-	y = *ElementFromString("2")
+	x = *ElementFromString("2")
+	y = *ElementFromString("3")
 	expected = *ElementFromString("6")
 
 	x.ToMont(&mod, &r_squared, modinv)
