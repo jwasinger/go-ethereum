a1bb11df31c0df953ee96d8d6c419ffa7eef9200
Jared Wasinger <j-wasinger@hotmail.com>
2021-10-07 11:08:03 +0200

    wip

--- eth/tracers/plugin_tracer.go
+++ eth/tracers/plugin_tracer.go
@@ -46,40 +46,46 @@ type PluginMemoryWrapper struct {
     memory *vm.Memory
 }
 
-func (p *PluginMemoryWrapper) Slice(begin, end int64) []byte {
-
+func (mw *PluginMemoryWrapper) Slice(begin, end int64) []byte {
+    if end == begin {
+        return []byte{}
+    }
+    if end < begin || begin < 0 {
+        log.Warn("Tracer accessed out of bound memory", "offset", begin, "end", end)
+        return nil
+    }
+    if mw.memory.Len() < int(end) {
+        log.Warn("Tracer accessed out of bound memory", "available", mw.memory.Len(), "offset", begin, "size", end-begin)
+        return nil
+    }
+    return mw.memory.GetCopy(begin, end-begin)
 }
 
-func (p *PluginMemoryWrapper) GetUint(addr uint64) *big.Int {
-
+func (mw *PluginMemoryWrapper) GetUint(addr uint64) *big.Int {
+    if mw.memory.Len() < int(addr)+32 || addr < 0 {
+        log.Warn("Tracer accessed out of bound memory", "available", mw.memory.Len(), "offset", addr, "size", 32)
+        return new(big.Int)
+    }
+    return new(big.Int).SetBytes(mw.memory.GetPtr(addr, 32))
 }
 
 type PluginStackWrapper struct {
-
+	stack *vm.Stack
 }
 
 func (s *PluginStackWrapper) Peek(idx int) *big.Int {
-
+    if len(sw.stack.Data()) <= idx || idx < 0 {
+        log.Warn("Tracer accessed out of bound stack", "size", len(sw.stack.Data()), "index", idx)
+        return new(big.Int)
+    }
+    return sw.stack.Back(idx).ToBig()
 }
 
-type PluginContractWrapper struct {
-
-}
-
-func (c *PluginContractWrapper) Caller() common.Address {
-
-}
-
-func (c *PluginContractWrapper) Address() common.Address {
-
-}
-
-func (c *PluginContractWrapper) Value() *big.Int {
-
-}
-
-func (c *PluginContractWrapper) Input() []byte {
-
+type PluginContract struct {
+	Caller common.Address
+	Address common.Address
+	Value *big.Int
+	Input []byte
 }
 
 type PluginTracer struct {
@@ -108,6 +114,7 @@ func NewPluginTracer(path string) (*PluginTracer, error) {
 
 func (t *PluginTracer) CaptureStart(env *vm.EVM, from common.Address, to common.Address, create bool, input []byte, gas uint64, value *big.Int) {
 	log.Info("PluginTracer.CaptureStart")
+	t.db = env.db
 }
 
 func (t *PluginTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, _ *vm.ScopeContext, rData []byte, depth int, err error) {
