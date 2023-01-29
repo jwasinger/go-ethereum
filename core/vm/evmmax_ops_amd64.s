e317eaf85bdf0682db1acd59751261eaffbd51db
Jared Wasinger <j-wasinger@hotmail.com>
2023-01-29 09:07:40 -0500

    wip

--- core/vm/evmmax_ops_amd64.s
+++ core/vm/evmmax_ops_amd64.s
@@ -1,8 +1,15 @@
 #include "textflag.h"
 
-TEXT ·opAddModX(SB),NOSPLIT,$0-24
+TEXT ·opAddModX(SB),NOSPLIT,$0-64
+    // *pc += 3
+    MOVQ    pc+8(SP), AX
+    ADDQ    $3, (AX)
+
+    // setup return
 	MOVQ    $0, 24(SP)
 	XORPS   X0, X0
 	MOVUPS  X0, 32(SP)
 	MOVUPS  X0, 48(SP)
+	MOVUPS  X0, 56(SP)
+	MOVUPS  X0, 64(SP)
 	RET
