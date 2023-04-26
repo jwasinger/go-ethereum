354f4e29a4d9a33351f77fa04882bef10800480b
Jared Wasinger <j-wasinger@hotmail.com>
2023-04-26 01:14:21 +0000

    .

--- elhook/elhook.go
+++ elhook/elhook.go
@@ -2,11 +2,31 @@ package elhook
 
 import (
 	"context"
-	"fmt"
+	"errors"
 )
 
 type ELClientHook struct {
 	engineAPI *EngineAPI
+}
+
+func checkCaps(caps []string) bool {
+	allMethods := requiredEngineAPIMethods()
+	for supportedMethod := range allMethods {
+		// TODO nested for-loop bad.  turn caps into a map
+		isSupported := false
+		for _, providedMethod := range caps {
+			if providedMethod == supportedMethod {
+				isSupported = true
+				break
+			}
+		}
+
+		if !isSupported {
+			panic(supportedMethod)
+			return false
+		}
+	}
+	return true
 }
 
 func (c *ELClientHook) Connect(ctx context.Context, httpEndpoint string) error {
@@ -16,13 +36,16 @@ func (c *ELClientHook) Connect(ctx context.Context, httpEndpoint string) error {
 		return err
 	}
 
+	// TODO: we provide one method and it returns all supported ones right?
 	capsRequested := []string{"engine_newPayloadV1"}
 	caps, err := engineAPI.ExchangeCapabilities(ctx, capsRequested)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	fmt.Println(caps)
+	if !checkCaps(caps) {
+		return errors.New("doesn't have required capability")
+	}
 
 	return nil
 }
@@ -32,7 +55,17 @@ func (c *ELClientHook) Run() {
 		select {
 		// newHead:
 		// 	if not received from EL client, send forkChoiceUpdated to EL
+		//	
+		//	if we can sign:
+		//		send getPayload to EL
+		//		if we aren't in-turn: jiggle and then report the block
+		// timer:
+		//	if we can sign:
+		//		send getPayload to EL
+		//		if we aren't in-turn: jiggle and then report the block
 		// 
+		// quit:
+		//
 		}
 	}
 }
--- elhook/engine_api.go
+++ elhook/engine_api.go
@@ -14,7 +14,28 @@ type EngineAPI struct {
 
 const (
 	exchangeCapabilitiesMethod = "engine_exchangeCapabilities"
+	forkchoiceUpdatedV2Method = "engine_forkchoiceUpdatedV2"
+	getPayloadV2Method = "engine_getPayloadV2"
+	getPayloadBodiesByHashV1Method = "engine_getPayloadBodiesByHashV1"
+	getPayloadBodiesByRangeV1Method = "engine_getPayloadBodiesByRangeV1"
+	newPayloadV1Method = "engine_newPayloadV1"
+	forkchoiceUpdatedV1Method = "engine_forkchoiceUpdatedV1"
+	getPayloadV1Method = "engine_getPayloadV1"
+	exchangeTransitionConfigurationV1 = "engine_exchangeTransitionConfigurationV1"
 )
+
+func requiredEngineAPIMethods() map[string]struct{} {
+	return map[string]struct{} {
+		forkchoiceUpdatedV2Method: struct{}{},
+		getPayloadV2Method: struct{}{},
+		getPayloadBodiesByHashV1Method: struct{}{},
+		getPayloadBodiesByRangeV1Method: struct{}{},
+		newPayloadV1Method: struct{}{},
+		forkchoiceUpdatedV1Method: struct{}{},
+		getPayloadV1Method: struct{}{},
+		exchangeTransitionConfigurationV1: struct{}{},
+	}
+}
 
 func NewWithConnection(ctx context.Context) (*EngineAPI, error) {
 	var testSecret = [32]byte{94, 111, 36, 109, 245, 74, 43, 72, 202, 33, 205, 86, 199, 174, 186, 77, 165, 99, 13, 225, 149, 121, 125, 249, 128, 109, 219, 163, 224, 176, 46, 233}
@@ -32,13 +53,12 @@ func NewWithConnection(ctx context.Context) (*EngineAPI, error) {
 }
 
 func (e *EngineAPI) ExchangeCapabilities(ctx context.Context, capsRequested []string) ([]string, error) {
-	var result *[]string
+	var result []string
 
 	err := e.client.CallContext(ctx, &result, exchangeCapabilitiesMethod, capsRequested)
 	if err != nil {
-		fmt.Println("error")
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return *result, nil
+	return result, nil
 }
