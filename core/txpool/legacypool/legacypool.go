af6080e6654f82ee88bfe8176385ab156e05cc0f
Jared Wasinger <j-wasinger@hotmail.com>
2024-09-23 20:41:31 +0700

    try better way

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -435,13 +435,24 @@ func (pool *LegacyPool) SetGasTip(tip *big.Int) {
 	pool.mu.Lock()
 	defer pool.mu.Unlock()
 
-	var (
-		newTip = uint256.MustFromBig(tip)
-		old    = pool.gasTip.Load()
-	)
-	pool.gasTip.Store(newTip)
+	newTip := uint256.MustFromBig(tip)
 	// If the min miner fee increased, remove transactions below the new threshold
-	if newTip.Cmp(old) > 0 {
+	pool.flushTransactionsBelowTip(tip)
+	pool.gasTip.Store(newTip)
+	log.Info("Legacy pool tip threshold updated", "tip", newTip)
+}
+
+func (pool *LegacyPool) FlushAllTransactions() {
+	maxUint256 := new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 256), common.Big1)
+	pool.mu.Lock()
+	defer pool.mu.Unlock()
+	pool.flushTransactionsBelowTip(maxUint256)
+}
+
+// assumes pool mutex is held
+func (pool *LegacyPool) flushTransactionsBelowTip(tip *big.Int) {
+	curMinTip := pool.gasTip.Load()
+	if uint256.MustFromBig(tip).Cmp(curMinTip) > 0 {
 		// pool.priced is sorted by GasFeeCap, so we have to iterate through pool.all instead
 		drop := pool.all.RemotesBelowTip(tip)
 		for _, tx := range drop {
@@ -449,7 +460,6 @@ func (pool *LegacyPool) SetGasTip(tip *big.Int) {
 		}
 		pool.priced.Removed(len(drop))
 	}
-	log.Info("Legacy pool tip threshold updated", "tip", newTip)
 }
 
 // Nonce returns the next nonce of an account, with all transactions executable
