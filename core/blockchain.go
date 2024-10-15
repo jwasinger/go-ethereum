641277ad7d37ca859492ea2aecd4b5ea8f5b3953
Jared Wasinger <j-wasinger@hotmail.com>
2024-10-15 17:30:07 +0700

    experiment: non-parallel tx sender recovery in blockchain import

--- core/blockchain.go
+++ core/blockchain.go
@@ -1617,8 +1617,12 @@ func (bc *BlockChain) insertChain(chain types.Blocks, setHead bool, makeWitness 
 	if bc.insertStopped() {
 		return nil, 0, nil
 	}
-	// Start a parallel signature recovery (signer will fluke on fork transition, minimal perf loss)
-	SenderCacher.RecoverFromBlocks(types.MakeSigner(bc.chainConfig, chain[0].Number(), chain[0].Time()), chain)
+	signer := types.MakeSigner(bc.chainConfig, chain[0].Number(), chain[0].Time())
+	for _, block := range chain {
+		for _, tx := range block.Transactions() {
+			signer.Sender(tx)
+		}
+	}
 
 	var (
 		stats     = insertStats{startTime: mclock.Now()}
