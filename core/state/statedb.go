2e6b7e3cd6cf3cd2c5c9c2783af9481374001dbb
Jared Wasinger <j-wasinger@hotmail.com>
2025-06-10 17:36:16 +0200

    add bal equality helper func

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -208,7 +208,10 @@ func (a *accountAccess) MarkWrite(txIdx uint64, key, value common.Hash) {
 }
 
 // map of transaction idx to the new code
-type codeDiff map[uint64][]byte
+type codeDiff struct {
+	txIdx uint64
+	code  []byte
+}
 
 type balanceDiff map[uint64]*uint256.Int
 
@@ -220,6 +223,112 @@ type bal struct {
 	codeChanges     map[common.Address]codeDiff
 	prestateNonces  map[common.Address]nonceDiff
 	balanceChanges  map[common.Address]balanceDiff
+}
+
+func (b *bal) eq(other *bal) bool {
+
+	// check that the account accesses are equal (consider moving this into its own function)
+
+	if len(b.accountAccesses) != len(other.accountAccesses) {
+		return false
+	}
+	for address, aa := range b.accountAccesses {
+		otherAA, ok := other.accountAccesses[address]
+		if !ok {
+			return false
+		}
+		if len(aa.accesses) != len(otherAA.accesses) {
+			return false
+		}
+		for key, vals := range aa.accesses {
+			otherAccesses, ok := otherAA.accesses[key]
+			if !ok {
+				return false
+			}
+			if len(vals.writes) != len(otherAccesses.writes) {
+				return false
+			}
+
+			for i, writeVal := range vals.writes {
+				otherWriteVal, ok := otherAccesses.writes[i]
+				if !ok {
+					return false
+				}
+				if writeVal != otherWriteVal {
+					return false
+				}
+			}
+		}
+	}
+
+	// check that the code changes are equal
+
+	if len(b.codeChanges) != len(other.codeChanges) {
+		return false
+	}
+	for addr, codeCh := range b.codeChanges {
+		otherCodeCh, ok := other.codeChanges[addr]
+		if !ok {
+			return false
+		}
+		if codeCh.txIdx != otherCodeCh.txIdx {
+			return false
+		}
+		if bytes.Compare(codeCh.code, otherCodeCh.code) != 0 {
+			return false
+		}
+	}
+
+	if len(b.prestateNonces) != len(other.prestateNonces) {
+		return false
+	}
+	for addr, nonces := range b.prestateNonces {
+		otherNonces, ok := other.prestateNonces[addr]
+		if !ok {
+			return false
+		}
+
+		if len(nonces) != len(otherNonces) {
+			return false
+		}
+
+		for txIdx, nonce := range nonces {
+			otherNonce, ok := otherNonces[txIdx]
+			if !ok {
+				return false
+			}
+			if nonce != otherNonce {
+				return false
+			}
+		}
+	}
+
+	if len(b.balanceChanges) != len(other.balanceChanges) {
+		return false
+	}
+
+	for addr, balanceChanges := range b.balanceChanges {
+		otherBalanceChanges, ok := other.balanceChanges[addr]
+		if !ok {
+			return false
+		}
+
+		if len(balanceChanges) != len(otherBalanceChanges) {
+			return false
+		}
+
+		for txIdx, balanceCh := range balanceChanges {
+			otherBalanceCh, ok := otherBalanceChanges[txIdx]
+			if !ok {
+				return false
+			}
+
+			if balanceCh != otherBalanceCh {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 // called during tx finalisation for each dirty account with changed nonce (whether by being the sender of a tx or calling CREATE)
@@ -277,9 +386,12 @@ func (b *bal) StorageWrite(account *stateObject, txIdx uint64, key, value common
 // called during tx finalisation for each dirty account with mutated code
 func (b *bal) CodeChange(txIdx uint64, account *stateObject) {
 	if _, ok := b.codeChanges[account.address]; !ok {
-		b.codeChanges[account.address] = make(codeDiff)
+		b.codeChanges[account.address] = codeDiff{}
 	}
-	b.codeChanges[account.address][txIdx] = bytes.Clone(account.Code())
+	b.codeChanges[account.address] = codeDiff{
+		txIdx,
+		bytes.Clone(account.Code()),
+	}
 }
 
 // NewWithReader creates a new state for the specified state root. Unlike New,
