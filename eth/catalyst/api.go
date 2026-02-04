5ac1f170adaead6477a7676dda9df9b2d47506fa
Jared Wasinger <j-wasinger@hotmail.com>
2026-02-03 19:05:16 -0500

    return block access lists in GetPayloadBodies*V2

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -1187,6 +1187,10 @@ func getBody(block *types.Block) *engine.ExecutionPayloadBody {
 		result.Withdrawals = []*types.Withdrawal{}
 	}
 
+	if block.AccessList() != nil {
+		result.AccessList = block.AccessList()
+	}
+
 	return &result
 }
 
