963b446f572a73b3ea21dbfafd47d8526418924e
Sahil Sojitra <88416181+Sahil-4555@users.noreply.github.com>
2026-03-13 23:34:04 +0530

    rpc: add blockAccessListHash to RPC block header response (#33994)
    Adds support for returning `blockAccessListHash` in the RPC block header
    response.
    
    This change includes `blockAccessListHash` in `RPCMarshalHeader` when
    `head.BlockAccessListHash` is not nil, aligning the RPC output with the
    Header struct which already contains this field.

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -985,6 +985,9 @@ func RPCMarshalHeader(head *types.Header) map[string]interface{} {
 	if head.RequestsHash != nil {
 		result["requestsHash"] = head.RequestsHash
 	}
+	if head.BlockAccessListHash != nil {
+		result["blockAccessListHash"] = head.BlockAccessListHash
+	}
 	if head.SlotNumber != nil {
 		result["slotNumber"] = hexutil.Uint64(*head.SlotNumber)
 	}
